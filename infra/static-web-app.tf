36dd77da891845c33f1b5152f357cad51c556a61
Liam <liamgoodchild12@hotmail.co.uk>
2026-06-26 16:27:34 +0100

    Use Free tier preconfigured Entra auth not custom

--- infra/static-web-app.tf
+++ infra/static-web-app.tf
@@ -2,8 +2,8 @@ resource "azurerm_static_web_app" "main" {
   name                = "stapp-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.main.name
   location            = "westeurope"
-  sku_tier            = "Standard"
-  sku_size            = "Standard"
+  sku_tier            = "Free"
+  sku_size            = "Free"
 
   app_settings = {
     ENTRA_CLIENT_ID     = var.entra_api_client_id
